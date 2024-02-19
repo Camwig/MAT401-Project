b1d022cfb66a9621f971615498a7382f87f41c33
Camwig <2001551@uad.ac.uk>
2024-02-19 13:15:42 +0000

    Managed to write to a csv file
    Now I just need to figure out to write to it as a table of contents. The multiple itteration thing can just be done by holding the data in some kind of container.

--- MAT401_Project/MAT401_Project/Source.cpp
+++ MAT401_Project/MAT401_Project/Source.cpp
@@ -5,5 +5,13 @@
 int main()
 {
 	std::printf("Hello World\n");
+
+	std::ofstream my_file("test.csv");
+
+	if (my_file.is_open()) {
+		my_file << ("Cheese") << "," << std::endl;
+	}
+
+	my_file.close();
 	return 0;
 }
