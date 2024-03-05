b568899bef00d8c283a97971722070ed169ec13f
Camwig <2001551@uad.ac.uk>
2024-03-05 15:35:55 +0000

    First attempt at Task 1
    Not sure if it works yet or not so Im going to have to test it against values I get by doing it on paper.

--- MAT401_Project/MAT401_Project/Task_1_and_2.cpp
+++ MAT401_Project/MAT401_Project/Task_1_and_2.cpp
@@ -2,12 +2,58 @@
 
 Task_1_and_2::Task_1_and_2()
 {
-
+	maths_ = new math_library();
 }
 
 Task_1_and_2::~Task_1_and_2()
 {
+	maths_->~math_library();
+	maths_ = nullptr;
+	delete maths_;
+}
 
+std::array<float,3> Task_1_and_2::RK4_Task1(std::array<float, 3>& Upsilon, float Omega_x, float Omega_y,float Omega_z,float dx)
+{
+	//Will need a k1 to k4 for x,y and z.
+	float k1_x = dx * Equation(Upsilon[0], Omega_y, Omega_z);
+	float k1_y = dx * Equation(Upsilon[1], Omega_x, Omega_z);
+	float k1_z = dx * Equation(Upsilon[2], Omega_x, Omega_y);
+
+	float k2_x = dx * Equation(Upsilon[0], Omega_y + (k1_y / 2.0f), Omega_z + /*0.1f**/(k1_z / 2.0f));
+	float k2_y = dx * Equation(Upsilon[1], Omega_x + (k1_x / 2.0f), Omega_z + /*0.1f**/(k1_z / 2.0f));
+	float k2_z = dx * Equation(Upsilon[2], Omega_x + (k1_x / 2.0f), Omega_y + /*0.1f**/(k1_y / 2.0f));
+
+	float k3_x = dx * Equation(Upsilon[0], Omega_y + (k2_y / 2.0f), Omega_z + /*0.1f**/(k2_z / 2.0f));
+	float k3_y = dx * Equation(Upsilon[1], Omega_x + (k2_x / 2.0f), Omega_z + /*0.1f**/(k2_z / 2.0f));
+	float k3_z = dx * Equation(Upsilon[2], Omega_x + (k2_x / 2.0f), Omega_y + /*0.1f**/(k2_y / 2.0f));
+
+	float k4_x = dx * Equation(Upsilon[0], Omega_y + (k3_y), Omega_z + /*0.1f**/(k3_z));
+	float k4_y = dx * Equation(Upsilon[1], Omega_x + (k3_x), Omega_z + /*0.1f**/(k3_z));
+	float k4_z = dx * Equation(Upsilon[2], Omega_x + (k3_x), Omega_y + /*0.1f**/(k3_y));
+
+	float Ouptut_x = Omega_x + (1.0f / 6.0f * (k1_x + (2.0f * k2_x) + (2.0f * k3_x) + k4_x));
+	float Ouptut_y = Omega_x + (1.0f / 6.0f * (k1_y + (2.0f * k2_y) + (2.0f * k3_y) + k4_y));
+	float Ouptut_z = Omega_x + (1.0f / 6.0f * (k1_z + (2.0f * k2_z) + (2.0f * k3_z) + k4_z));
+
+	std::array<float, 3> output;
+	output[0] = Ouptut_x;
+	output[1] = Ouptut_y;
+	output[2] = Ouptut_z;
+
+	return output;
+
+
+	//float k2 = dx * Equation(Upsilon, Omega_1 + dx / 2.0f, Omega_2 + /*0.1f**/(k1 / 2.0f));
+
+	//float k3 = dx * function(x[i - 1] + dx / 2.0f, y[i - 1] + /*0.1f**/(k2 / 2.0f));
+
+	//float k4 = dx * function(x[i - 1] + dx, y[i - 1] + /*0.1f**/(k2));
+
+	//std::cout << y[i] << "\n";
+
+	//float output = y[i - 1] + (1.0f / 6.0f * (k1 + (2.0f * k2) + (2.0f * k3) + k4));
+
+	//return output;
 }
 
 void Task_1_and_2::Init(float M,float R, float H)
@@ -32,4 +78,36 @@ void Task_1_and_2::Init(float M,float R, float H)
 	Upsilon[0] = (zz - xx_yy) / xx_yy;
 	Upsilon[1] = (xx_yy - zz) / xx_yy;
 	Upsilon[2] = (xx_yy - xx_yy) / zz;
+}
+
+void Task_1_and_2::Solve_Task_1(std::vector<std::vector<float>>& Out_X, std::vector<std::vector<float>>& Out_Y, std::vector<std::vector<float>>& Out_Z,float Initial_X,float Initial_Y,float Initial_Z,float Step,float Start,float End)
+{
+	std::vector<float> Times = maths_->Generate_Half_open_interval(Step, Start, End);
+
+	//Set the intial value for these
+	Omega_X = maths_->Generate_zeros(End);
+	Omega_Y = maths_->Generate_zeros(End);
+	Omega_Z = maths_->Generate_zeros(End);
+
+	Omega_X[0] = Initial_X;
+	Omega_Y[0] = Initial_Y;
+	Omega_Z[0] = Initial_Z;
+
+	std::array<float, 3> Results;
+
+
+	for (float i = Start; i <= End; i += Step)
+	{
+		//RK4 Step which can then be given to the Output vectors
+
+		Results = RK4_Task1(Upsilon, Omega_X[i - 1], Omega_Y[i - 1], Omega_Z[i - 1], Step);
+
+		Omega_X[i] = Results[0];
+		Omega_Y[i] = Results[1];
+		Omega_Z[i] = Results[2];
+	}
+
+	Out_X = { Omega_X,Times };
+	Out_Y = { Omega_Y,Times };
+	Out_Z = { Omega_Z,Times };
 }
--- MAT401_Project/MAT401_Project/Task_1_and_2.h
+++ MAT401_Project/MAT401_Project/Task_1_and_2.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <vector>
 #include <array>
+#include "math_library.h"
 
 class Task_1_and_2
 {
@@ -10,12 +12,26 @@ public:
 
 	void Init(float, float, float);
 
+	void Solve_Task_1(std::vector<std::vector<float>>&, std::vector<std::vector<float>>&, std::vector<std::vector<float>>&,float,float,float,float,float,float);
 private:
+
+	std::array<float, 3> RK4_Task1(std::array<float,3>&, float, float,float,float);
+
+	float inline Equation(float upslion, float Omega_1, float Omega_2)
+	{
+		return -1 * (upslion * Omega_1 * Omega_2);
+	}
 
 	std::array<std::array<float, 3>, 3> Inertia_Tensor;
 
 	std::array<float, 3> Centre_of_mass;
 
 	std::array<float, 3> Upsilon;
+
+	std::vector<float> Omega_X;
+	std::vector<float> Omega_Y;
+	std::vector<float> Omega_Z;
+
+	math_library* maths_;
 };
 
