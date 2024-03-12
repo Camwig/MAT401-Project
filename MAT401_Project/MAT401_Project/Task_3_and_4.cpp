2da498933d5fb58b82e5a47d8ad32208e8831887
Camwig <2001551@uad.ac.uk>
2024-03-12 14:17:26 +0000

    Ive created a set of code for task 3 and 4
    Now much like task 1 and 2 I need to check if the first four iterations of bath sets of tasks output the correct values. Then I will have to make sure they plot okay.

--- MAT401_Project/MAT401_Project/Task_3_and_4.cpp
+++ MAT401_Project/MAT401_Project/Task_3_and_4.cpp
@@ -14,13 +14,55 @@ Task_3_and_4::~Task_3_and_4()
 
 void Task_3_and_4::Init(std::array<float, 3> Initial_velocity, std::array<float, 3> Initial_position)
 {
-	velocity[0] = Initial_velocity[0];
-	velocity[1] = Initial_velocity[1];
-	velocity[2] = Initial_velocity[2];
+	velocity[0][0] = Initial_velocity[0];
+	velocity[1][0] = Initial_velocity[1];
+	velocity[2][0] = Initial_velocity[2];
 
-	position[0] = Initial_position[0];
-	position[1] = Initial_position[1];
-	position[2] = Initial_position[2];
+	position[0][0] = Initial_position[0];
+	position[1][0] = Initial_position[1];
+	position[2][0] = Initial_position[2];
 }
 
-//Do the same kind of setup as task_1 but use the semi-implicit euler method instead of runga-kutta
+float Task_3_and_4::Semi_Implict_Euler(float Initial_Value, float Diffrence, float Step)
+{
+	float Output = 0.0f;
+	Output = Initial_Value + (Diffrence * Step);
+	return Output;
+}
+
+void Task_3_and_4::Solve_Task_3(std::array<float, 3> Initial_velocity, std::array<float, 3> Initial_position,std::vector<std::vector<float>>& Out_Vx, std::vector<std::vector<float>>& Out_Vy, std::vector<std::vector<float>>& Out_Vz, std::vector<std::vector<float>>& Out_Px, std::vector<std::vector<float>>& Out_Py, std::vector<std::vector<float>>& Out_Pz, float Step, float Start, float End)
+{
+	std::vector<float> Times = maths_->Generate_Half_open_interval(Step, Start, End);
+
+	velocity[0] = maths_->Generate_zeros(End);
+	velocity[1] = maths_->Generate_zeros(End);
+	velocity[2] = maths_->Generate_zeros(End);
+
+	position[0] = maths_->Generate_zeros(End);
+	position[1] = maths_->Generate_zeros(End);
+	position[2] = maths_->Generate_zeros(End);
+
+	Init(Initial_velocity,Initial_position);
+
+
+	for (float i = Start; i <= End - 1; i += Step)
+	{
+		//Semi-implicit Euler
+		velocity[0][i] = Semi_Implict_Euler(velocity[0][i-1],Gravity,Step);
+		position[0][i] = Semi_Implict_Euler(position[0][i - 1], velocity[0][i], Step);
+
+		velocity[1][i] = Semi_Implict_Euler(velocity[1][i - 1], Gravity, Step);
+		position[1][i] = Semi_Implict_Euler(position[1][i - 1], velocity[1][i], Step);
+
+		velocity[2][i] = Semi_Implict_Euler(velocity[2][i - 1], Gravity, Step);
+		position[2][i] = Semi_Implict_Euler(position[2][i - 1], velocity[2][i], Step);
+	}
+
+	Out_Vx = { velocity[0],Times };
+	Out_Vy = { velocity[1],Times };
+	Out_Vz = { velocity[2],Times };
+
+	Out_Px = { position[0],Times };
+	Out_Py = { position[1],Times };
+	Out_Pz = { position[2],Times };
+}
--- MAT401_Project/MAT401_Project/Task_3_and_4.h
+++ MAT401_Project/MAT401_Project/Task_3_and_4.h
@@ -9,10 +9,13 @@ public:
 	~Task_3_and_4();
 
 	void Init(std::array<float,3> Initial_velocity, std::array<float, 3> Initial_position);
+	void Solve_Task_3(std::array<float, 3> Initial_velocity, std::array<float, 3> Initial_position,std::vector<std::vector<float>>& Out_Vx, std::vector<std::vector<float>>& Out_Vy, std::vector<std::vector<float>>& Out_Vz, std::vector<std::vector<float>>& Out_Px, std::vector<std::vector<float>>& Out_Py, std::vector<std::vector<float>>& Out_Pz, float Step, float Start, float End);
+
+	float Semi_Implict_Euler(float Initial_Value,float Diffrence,float Step);
 
 private:
-	std::array<float, 3> velocity;
-	std::array<float, 3> position;
+	std::array<std::vector<float>, 3> velocity;
+	std::array<std::vector<float>, 3> position;
 	const float Gravity = -9.8f;
 	math_library* maths_;
 };
